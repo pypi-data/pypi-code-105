e7ccdc22c746be7775c214cdd1c5fb0fd89c9795
Tom Forbes <tom@tomforb.es>
2021-12-26 09:13:25 +0000

    {"name":"apachecn-android-zh-pt2","version":"2021.12.26.0","file":"apachecn-android-zh-pt2-2021.12.26.0.tar.gz","path":"apachecn-android-zh-pt2/apachecn-android-zh-pt2-2021.12.26.0.tar.gz"}

---
+++ apachecn-android-zh-pt2/apachecn-android-zh-pt2-2021.12.26.0.tar.gz/ApachecnAndroidZhPt2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""apachecn-android-zh-pt2
+https://github.com/apachecn/apachecn-android-zh-pt2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.12.26.0"
